3e2ac493c813adcda5b3bddffced9123ddb7429e
Oliver@Oliver-PC <oliverferguson@hotmail.com>
2019-12-04 18:22:07 +0000

    Updates

---
+++ precache-manifest.d5ce069cba957ab96be19956ca1b6188.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21af23155b9ac0e20fdfd1fd1c3c3f05",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dc56f6e7d4433fcccadf",
+    "url": "/robofriends/static/css/main.2cf76553.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "dc56f6e7d4433fcccadf",
+    "url": "/robofriends/static/js/main.b3faee29.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
